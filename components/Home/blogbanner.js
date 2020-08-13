1cfe2960a88ad8c89e35560c279fb2bb989b4b38
Cyril Cabo <cyrilcabo@gmail.com>
2020-08-13 16:46:50 +0800

    Update blogbanner.js

--- components/Home/blogbanner.js
+++ components/Home/blogbanner.js
@@ -51,7 +51,7 @@ const useStyle = makeStyles(theme => ({
 			}
 		},
 		display: 'flex',
-		justifyContent: 'center',
+		justifyContent: 'flex-start',
 		alignItems: 'center',
 		[theme.breakpoints.down('sm')]: {
 			justifyContent: 'center',
@@ -94,6 +94,7 @@ const useStyle = makeStyles(theme => ({
 		backgroundColor: '#403e3c',
 		color: '#f1f1f1',
 		fontSize: '1.5rem',
+		fontWeight: 550,
 		[theme.breakpoints.down('sm')]: {
 			fontSize: '1rem',
 			marginBottom: 10,
@@ -107,16 +108,18 @@ const BlogBanner = (props) => {
 		<Grid item xs={12} container justify="space-around" className={classes.root}>
 			<div className={classes.bg} />
 			<BlogLeftDesign className={classes.leftDesign} preserveAspectRatio={"none"} />
-			<Grid item xs={12} md={5} className={classes.cup} >
-				<CoffeeCup preserveAspectRatio={"none"}/>
-			</Grid>
-			<Grid item xs={12} md={6} className={[classes.rightToCenter, classes.details].join(' ')}>
-				<Grid item className={classes.rightToCenter}>
-					<h1 className={classes.title}> Coffee break! </h1>
-					<p className={classes.subTitle}> Wanna get to know more of the artist? </p>
+			<Grid item xs={11} md={10} justify="space-around" container">
+				<Grid item xs={12} md={5} className={classes.cup} >
+					<CoffeeCup preserveAspectRatio={"none"}/>
 				</Grid>
-				<Grid item className={classes.rightToCenter} container>
-					<Button variant="contained" className={classes.button} onClick={() => Router.push('/blog')}> Read more </Button>
+				<Grid item xs={12} md={6} className={[classes.rightToCenter, classes.details].join(' ')}>
+					<Grid item className={classes.rightToCenter}>
+						<h1 className={classes.title}> Coffee break! </h1>
+						<p className={classes.subTitle}> Wanna get to know more of the artist? </p>
+					</Grid>
+					<Grid item className={classes.rightToCenter} container>
+						<Button variant="contained" className={classes.button} onClick={() => Router.push('/blog')}> Read more </Button>
+					</Grid>
 				</Grid>
 			</Grid>
 		</Grid>
